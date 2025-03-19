3083da86eb9f65c90e1f359127c9520b5db9b602
wen2ms <1819509111@qq.com>
2025-03-19 16:24:43 +0800

    Feat: add Player's members

--- player.cpp
+++ player.cpp
@@ -1,3 +1,63 @@
 #include "player.h"
 
 Player::Player(QObject *parent) : QObject{parent} {}
+
+Player::Player(QString name, QObject *parent) {
+    name_ = name;
+}
+
+void Player::set_name(QString name) {
+    name_ = name;
+}
+
+QString Player::name() {
+    return name_;
+}
+
+void Player::set_role(Role role) {
+    role_ = role;
+}
+
+Player::Role Player::role() {
+    return role_;
+}
+
+void Player::set_sex(Sex sex) {
+    sex_ = sex;
+}
+
+Player::Sex Player::sex() {
+    return sex_;
+}
+
+void Player::set_direction(Direction direction) {
+    direction_ = direction;
+}
+
+Player::Direction Player::direction() {
+    return direction_;
+}
+
+void Player::set_type(Type type) {
+    type_ = type;
+}
+
+Player::Type Player::type() {
+    return type_;
+}
+
+void Player::set_score(int score) {
+    score_ = score;
+}
+
+int Player::score() {
+    return score_;
+}
+
+void Player::set_win(bool flag) {
+    is_win_ = flag;
+}
+
+bool Player::is_win() {
+    return is_win_;
+}
--- player.h
+++ player.h
@@ -6,9 +6,45 @@
 class Player : public QObject {
     Q_OBJECT
   public:
+    enum Role {kLord, kFarmer};
+    enum Sex {kMale, kFemale};
+    enum Direction {kLeft, kRight};
+    enum Type {kRobot, kUser, kUnknown};
+    
     explicit Player(QObject *parent = nullptr);
-
+    explicit Player(QString name, QObject* parent = nullptr);
+    
+    void set_name(QString name);
+    QString name();
+    
+    void set_role(Role role);
+    Role role();
+    
+    void set_sex(Sex sex);
+    Sex sex();
+    
+    void set_direction(Direction direction);
+    Direction direction();
+    
+    void set_type(Type type);
+    Type type();
+    
+    void set_score(int score);
+    int score();
+    
+    void set_win(bool flag);
+    bool is_win();
+    
   signals:
+    
+  private:
+    int score_;
+    QString name_;
+    Role role_;
+    Sex sex_;
+    Direction direction_;
+    Type type_;
+    bool is_win_;
 };
 
 #endif  // PLAYER_H
