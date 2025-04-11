9555675d1b1ba204f2949fa7580572dd91aad156
wen2ms <1819509111@qq.com>
2025-04-12 00:12:39 +0800

    Feat: add EndPanel class

--- scorepanel.h
+++ scorepanel.h
@@ -2,6 +2,7 @@
 #define SCOREPANEL_H
 
 #include <QWidget>
+#include <QLabel>
 
 namespace Ui {
 class ScorePanel;
@@ -11,13 +12,21 @@ class ScorePanel : public QWidget {
     Q_OBJECT
 
   public:
+    enum FontColor {kBlack, kWhite, kRed, kBlue, kGreen};
+    
     explicit ScorePanel(QWidget *parent = nullptr);
     ~ScorePanel();
     
     void set_scores(int left_score, int right_score, int my_score);
+    
+    void set_score_font_size(int point);
+    
+    void set_score_font_color(FontColor color);
 
   private:
     Ui::ScorePanel *ui;
+    
+    QVector<QLabel*> labels_list_;
 };
 
 #endif  // SCOREPANEL_H
