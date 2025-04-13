fbf3400470ff1fcd7a4dce0ec3bb82148e5b7810
wen2ms <1819509111@qq.com>
2025-04-13 21:23:30 +0800

    Feat: add bidding music in BGMControl

--- bgmcontrol.cpp
+++ bgmcontrol.cpp
@@ -11,19 +11,19 @@ BGMControl::BGMControl(QObject *parent) : QObject{parent} {
     for (int i = 0; i < 5; ++i) {
         QMediaPlayer* player = new QMediaPlayer(this);
         QAudioOutput* audio_output = new QAudioOutput(this);
-        media_current_index[i] = 0;
+        media_current_index_[i] = 0;
         
         if (i == 2) {
             connect(player, &QMediaPlayer::mediaStatusChanged, this, [=] (QMediaPlayer::MediaStatus status) {
-                media_current_index[i] = (media_current_index[i] + 1) % play_list_[i].size();
-                player->setSource(play_list_[i][media_current_index[i]]);
+                media_current_index_[i] = (media_current_index_[i] + 1) % play_list_[i].size();
+                player->setSource(play_list_[i][media_current_index_[i]]);
                 player->play();
             });
         }
         
         audio_output->setVolume(1);
         player->setAudioOutput(audio_output);
-        player->setSource(play_list_[i][media_current_index[i]]);
+        player->setSource(play_list_[i][media_current_index_[i]]);
         
         media_player_.push_back(player);
         audio_output_.push_back(audio_output);
@@ -50,4 +50,35 @@ void BGMControl::init_play_list() {
             play_list_[i].push_back(QUrl(array.at(j).toString()));
         }
     }
+}
+
+void BGMControl::start_bgm(int volume) {
+    media_current_index_[2] = 0;
+    audio_output_[2]->setVolume(volume);
+    media_player_[2]->setSource(play_list_[2][media_current_index_[2]]);
+    media_player_[2]->play();
+}
+
+void BGMControl::stop_bgm() {
+    media_player_[2]->stop();
+}
+
+void BGMControl::player_bidding_music(int points, PlayerSex sex, bool is_first) {
+    int index = (sex == PlayerSex::kMale ? 0 : 1);
+    if (is_first && points > 0) {
+        media_current_index_[index] = CardType::kOrder;
+    } else if (points == 0) {
+        if (is_first) {
+            media_current_index_[index] = CardType::kNoOrder;
+        } else {
+            media_current_index_[index] = CardType::kNoRob;
+        }
+    } else if (points == 2) {
+        media_current_index_[index] = CardType::kRob1;
+    } else if (points == 3) {
+        media_current_index_[index] = CardType::kRob2;
+    }
+    
+    media_player_[index]->setSource(play_list_[index][media_current_index_[index]]);
+    media_player_[index]->play();
 }
--- bgmcontrol.h
+++ bgmcontrol.h
@@ -8,9 +8,89 @@
 class BGMControl : public QObject {
     Q_OBJECT
   public:
+    enum PlayerSex {kMale, kFemale};
+    
+    enum CardType
+    {
+        kThree,
+        kFoue,
+        kFive,
+        kSix,
+        kSeven,
+        kEight,
+        kNine,
+        kTen,
+        kJack,
+        kQueen,
+        kKing,
+        kAce,
+        kTow,
+        kSmallJoker,
+        kBigJoker,
+
+        kThreeDouble,
+        kFoueDouble,
+        FiveDouble,
+        kSixDouble,
+        kSevenDouble,
+        kEightDouble,
+        kNineDouble,
+        kTenDouble,
+        kJackDouble,
+        kQueenDouble,
+        kKingDouble,
+        kAceDouble ,
+        kTowDouble,
+        
+        kThreeTriple,
+        kFoueTriple,
+        kFiveTriple,
+        kSixTriple,
+        kSevenTriple,
+        kEightTriple,
+        kNineTriple,
+        kTenTriple,
+        kJackTriple,
+        kQueenTriple,
+        kKingTriple,
+        kAceTriple,
+        kTowTriple,
+        
+        kPlane,
+        kSequencePair,
+        kThreeBindOne,
+        kThreeBindPair,
+        kSequence,
+        kFourBindTwo,
+        kFourBind2Pair,
+        kBomb,
+        kJokerBomb,
+        kPass1,
+        kPass2,
+        kPass3,
+        kPass4,
+        kMoreBiger1,
+        kMoreBiger2,
+        kBiggest,
+
+        kNoOrder,
+        kNoRob, 
+        kOrder,
+        kRob1, 
+        kRob2,
+        kLast1, 
+        kLast2
+    };
+        
     explicit BGMControl(QObject *parent = nullptr);
     
     void init_play_list();
+    
+    void start_bgm(int volume);
+    
+    void stop_bgm();
+    
+    void player_bidding_music(int points, PlayerSex sex, bool is_first);
 
   signals:
     
@@ -18,7 +98,7 @@ class BGMControl : public QObject {
     QVector<QMediaPlayer*> media_player_;
     QVector<QAudioOutput*> audio_output_;
     QVector<QVector<QUrl>> play_list_;
-    QVector<int> media_current_index;    
+    QVector<int> media_current_index_;    
 };
 
 #endif  // BGMCONTROL_H
