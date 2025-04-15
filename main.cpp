63dfc7c40648af4d68d4c16aceb656f6e5859867
wen2ms <1819509111@qq.com>
2025-04-15 17:40:17 +0800

    Feat: implement loading widget

--- main.cpp
+++ main.cpp
@@ -1,10 +1,10 @@
 #include <QApplication>
 
-#include "mainwindow.h"
+#include "loading.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
-    MainWindow w;
+    Loading w;
     w.show();
     return a.exec();
 }
