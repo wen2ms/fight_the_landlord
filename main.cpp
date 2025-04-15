60272340d9b285a0c6f82b45e63e2d56058acdb3
wen2ms <1819509111@qq.com>
2025-04-15 22:11:56 +0800

    Feat: use rcc file

--- main.cpp
+++ main.cpp
@@ -1,9 +1,12 @@
 #include <QApplication>
+#include <QResource>
 
 #include "loading.h"
 
 int main(int argc, char *argv[]) {
     QApplication a(argc, argv);
+    QString rcc_path = QCoreApplication::applicationDirPath() + "/../Resources/resource.rcc";
+    QResource::registerResource(rcc_path);
     Loading w;
     w.show();
     return a.exec();
